a905fcb500ea480653dccb27b2cd9f8bc2f11aca
tonytech83 <a.petrov.83@gmail.com>
2024-10-06 14:59:48 +0300

    add creation od warranty

--- warranty_keeper/warranties/urls.py
+++ warranty_keeper/warranties/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from warranty_keeper.warranties.views import WarrantiesView
+from warranty_keeper.warranties.views import WarrantiesView, WarrantyCreateView, WarrantyUpdateView
 
 urlpatterns = (
     path("", WarrantiesView.as_view(), name="warranty-list"),
+    path('create/', WarrantyCreateView.as_view(), name='warranty-create'),
+    path('<int:pk>/edit/', WarrantyUpdateView.as_view(), name='warranty-update'),
 )
--- warranty_keeper/warranties/views.py
+++ warranty_keeper/warranties/views.py
@@ -1,8 +1,41 @@
+from django.urls import reverse_lazy
 from django.views import generic as views
 from django.contrib.auth import mixins as auth_mixins
 
 from warranty_keeper.warranties.models import Warranty
 
+
 class WarrantiesView(auth_mixins.LoginRequiredMixin, views.ListView):
-  queryset = Warranty.objects.all()
-  template_name = 'warranties/warranty-list.html'
+    queryset = Warranty.objects.all()
+    template_name = "warranties/warranty-list.html"
+
+
+class DetailsWarrantyView(views.DetailView):
+    pass
+
+
+class WarrantyCreateView(views.CreateView):
+    model = Warranty
+    fields = ["name", "description", "invoice_img", "supplier"]
+    template_name = "warranties/warranty-create.html"
+    success_url = reverse_lazy(
+        "warranty-list"
+    )  # Adjust this to your desired redirect URL
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class WarrantyUpdateView(views.UpdateView):
+    model = Warranty
+    fields = ["name", "description", "invoice_img", "supplier"]
+    template_name = "warranties/warranty_form.html"
+    success_url = reverse_lazy("warranty-list")
+
+    def get_queryset(self):
+        return Warranty.objects.filter(owner=self.request.user)
+
+
+class DeleteWarrantyView(views.DeleteView):
+    pass
