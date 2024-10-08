4e3baa4d1b8b22d5fef4385f4b1ee8d7eab5b4d9
tonytech83 <a.petrov.83@gmail.com>
2024-10-08 11:04:42 +0300

    add purchase_date and period fields for warranty model

--- warranty_keeper/warranties/models.py
+++ warranty_keeper/warranties/models.py
@@ -3,24 +3,47 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
 
 from warranty_keeper.core.model_mixins import TimeStampedModel
-from warranty_keeper.warranties.validators import validate_mime_type
+from warranty_keeper.warranties.validators import (
+    validate_mime_type,
+    validate_purchase_date,
+)
 from warranty_keeper.suppliers.models import Supplier
 
 
 UserModel = get_user_model()
 
-class Warranty(TimeStampedModel,models.Model):
+
+class Period(models.IntegerChoices):
+    ONE_MONTH = 1, "1 Month"
+    THREE_MONTHS = 3, "3 Months"
+    SIX_MONTHS = 6, "6 Months"
+    NINE_MONTHS = 9, "9 Months"
+    TWELVE_MONTHS = 12, "12 Months"
+    TWENTY_FOUR_MONTHS = 24, "24 Months"
+    THIRTY_SIX_MONTHS = 36, "36 Months"
+
+
+class Warranty(TimeStampedModel, models.Model):
     MAX_NAME_LENGTH = 100
     MIN_NAME_LENGTH = 2
 
     name = models.CharField(
         max_length=MAX_NAME_LENGTH,
-        validators=(
-            MinLengthValidator(MIN_NAME_LENGTH),
-        ),
+        validators=(MinLengthValidator(MIN_NAME_LENGTH),),
         unique=False,
         null=False,
         blank=False,
+    )
+
+    purchase_date = models.DateField(
+        validators=(validate_purchase_date,),
+        null=False,
+        blank=False,
+    )
+
+    period = models.IntegerField(
+        choices=Period.choices,
+        default=Period.TWENTY_FOUR_MONTHS,
     )
 
     description = models.TextField(
@@ -29,10 +52,8 @@ class Warranty(TimeStampedModel,models.Model):
     )
 
     invoice_img = models.FileField(
-        upload_to='invoices/',
-        validators=(
-            validate_mime_type,
-        ),
+        upload_to="invoices/",
+        validators=(validate_mime_type,),
         null=True,
         blank=True,
     )
--- warranty_keeper/warranties/validators.py
+++ warranty_keeper/warranties/validators.py
@@ -1,6 +1,7 @@
 import mimetypes
 
 from django.core.exceptions import ValidationError
+from django.utils.timezone import now
 
 def validate_mime_type(value):
     mime_type, _ = mimetypes.guess_type(value.name)
@@ -8,3 +9,7 @@ def validate_mime_type(value):
 
     if mime_type not in allowed_mime_types:
         raise ValidationError('Unsupported file type.')
+    
+def validate_purchase_date(value):
+    if value > now().date():
+        raise ValidationError("Purchase date cannot be in the future.")
